3cd1106df542053655802d55b108c588de49c8a2
StefanoTrv <stefanotravasci@gmail.com>
2021-08-02 19:41:31 +0200

    Completed initial implementation of the ICD tree Completed the code that creates the tree of the ICD10 classification from the XML file. The tree needs more testing and will be modified when the class is furtherly developed.

--- src/ICD10CodesManipulator.java
+++ src/ICD10CodesManipulator.java
@@ -1,5 +1,4 @@
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.*;
 import java.io.*;
@@ -15,47 +14,51 @@ public class ICD10CodesManipulator {
     private HashMap<String, Integer> codeToIndexDictionary;
 
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
+    public static void main(String[] args) throws IOException {
+        ICD10CodesManipulator icd = new ICD10CodesManipulator();
+    }
 
-        Document document = builder.parse(new File( "data\\icd_10_v2019.xml" ));
+    public ICD10CodesManipulator() throws IOException {
+        Document document;
+        try{
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(new File( "data\\icd_10_v2019.xml" ));
+        } catch (Exception e){
+            throw new IOException(e);
+        }
+
 
         document.getDocumentElement().normalize();
 
         Element root = document.getDocumentElement();
 
-        System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
-        System.out.println("------");
-
-        Node ch = root.getFirstChild();
-
         Node childNode = root.getFirstChild();
 
-        ArrayList<Element> chapterList = new ArrayList<Element>();
-        while( childNode.getNextSibling()!=null ){
+        ArrayList<Element> chapterElementsList = new ArrayList<Element>();
+        while( childNode!=null ){
             if (childNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element childElement = (Element) childNode;
-                System.out.println(childElement.getAttribute("type"));
-                chapterList.add(childElement);
+                chapterElementsList.add(childElement);
             }
             childNode = childNode.getNextSibling();
         }
 
-        System.out.println("\n\n"+chapterList.get(0).getTagName()+"\n\n");
-        Node ch1child = chapterList.get(0).getFirstChild();
-
-
-        while( ch1child.getNextSibling()!=null ){
-            if (ch1child.getNodeType() == Node.ELEMENT_NODE) {
-                Element childElement = (Element) ch1child;
-                System.out.println(childElement.getTagName()+" - "+childElement.getAttribute("type"));
-            }
-            ch1child = ch1child.getNextSibling();
+        this.chapterList=new ArrayList<>();
+        for(Element chapter: chapterElementsList){
+            chapterList.add(new ICDNode(chapter));
         }
 
+        for(ICDNode chapter: chapterList){
+            printICDNode(chapter);
+        }
+    }
 
-
+    private void printICDNode(ICDNode node){
+        System.out.println(node.getName()+" - "+node.getDescription());
+        for(ICDNode child: node.getChildren()){
+            printICDNode(child);
+        }
     }
 
     private class ICDNode{
@@ -65,31 +68,53 @@ public class ICD10CodesManipulator {
         private ICDNode parent;
         private ArrayList<ICDNode> children;
 
-        public ICDNode(String name, String description, String type, ICDNode parent, ArrayList<ICDNode> children){
-            this.name=name;
-            this.description=description;
-            this.type=type;
-            this.parent=parent;
-            this.children= children;
+        private ICDNode(Element element){
+            this.type = element.getAttribute("type");
+            this.children = new ArrayList<>();
+            this.parent=null;
+
+            //reads all the data from the XML file and creates children nodes
+            String tag;
+            Node child = element.getFirstChild();
+            while(child!=null){
+                if (child.getNodeType() == Node.ELEMENT_NODE) {
+                    Element childElement = (Element) child;
+                    tag = childElement.getTagName();
+                    if(tag.equals("item")){//adds new child
+                        this.children.add(new ICDNode(childElement));
+                    }else if(tag.equals("name")){//sets the name
+                        this.name=childElement.getTextContent();
+                    }else if(tag.equals("description")){//sets the description
+                        this.description=childElement.getTextContent();
+                    }else{
+                        System.out.println("Found unknown tag \""+tag+"\", ignoring it...");
+                    }
+                }
+                child = child.getNextSibling();
+            }
+            //updates the children ICDNodes by setting their parent to this
+            for(ICDNode icdChild:this.children){
+                icdChild.parent=this;
+            }
         }
 
-        public String getName(){
+        private String getName(){
             return this.name;
         }
 
-        public String getDescription(){
+        private String getDescription(){
             return this.description;
         }
 
-        public String getType(){
+        private String getType(){
             return this.type;
         }
 
-        public ICDNode getParent(){
+        private ICDNode getParent(){
             return this.parent;
         }
 
-        public ArrayList<ICDNode> getChildren(){
+        private ArrayList<ICDNode> getChildren(){
             return this.children;
         }
     }
